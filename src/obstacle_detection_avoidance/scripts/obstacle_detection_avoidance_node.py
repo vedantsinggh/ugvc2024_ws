e092e6c597daf077afb1e1ece87c8751dab34d4f
kartik-walia <itzkartikeds3@gmail.com>
2024-06-02 11:48:33 +0530

    Added dynamic speed decreasing behaviour & log frequency in obstacle detection and avoidance script

--- src/obstacle_detection_avoidance/scripts/obstacle_detection_avoidance_node.py
+++ src/obstacle_detection_avoidance/scripts/obstacle_detection_avoidance_node.py
@@ -11,14 +11,20 @@ class ObstacleAvoidance:
 
         # Parameters 
         self.min_distance = rospy.get_param('~min_distance', 0.5)    # meters
+        self.max_distance = rospy.get_param('~max_distance', 3.0)    # meters, beyond which the robot moves at max speed
         self.forward_speed = rospy.get_param('~forward_speed', 0.2)    # meters per second 
         self.turn_speed = rospy.get_param('~turn_speed', 0.5)    # radians per second 
+        self.log_frequency = rospy.get_param('~log_frequency', 1.0)    # log frequnecy in seconds 
+
 
         # Publisher 
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
         # Subscriber 
         rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+
+        # For controlling log output frequency 
+        self.last_log_time = rospy.get_time()
         
     def scan_callback(self, data):
         # Initialize Twist message
@@ -28,24 +34,32 @@ class ObstacleAvoidance:
         angle_increment_deg = data.angle_increment * (180.0 / 3.141592653589793) 
 
         # Calculate the number of measurements per 360 degree scan 
-        num_measurements = 360 / angle_increment_deg
+        num_measurements = int(360 / angle_increment_deg)
 
-        rospy.loginfo(f"Angle increment (degrees): {angle_increment_deg:.3f}")
-        rospy.loginfo(f"Number of measurements per 360-degree scan: {num_measurements:.0f}")
+        try:
+            # Divide the scan into 3 regions : 30, 90, 30 degrees
+            # regions = {
+            #     'left': min(min(data.ranges[0:int(num_measurements*1/3)]), 10),
+            #     'front': min(min(data.ranges[int(num_measurements*1/3):int(num_measurements*2/3)]), 10),
+            #     'right': min(min(data.ranges[int(num_measurements*2/3):]), 10),
+            # }
+            regions = {
+                'left': min(min(data.ranges[0:int(num_measurements*1/12)]), 5),
+                'front': min(min(data.ranges[int(num_measurements*1/12):int(num_measurements*4/12)]), 5),
+                'right': min(min(data.ranges[int(num_measurements*4/12):int(num_measurements*5/12)]), 5),
+            }
 
-        # Divide the scan into 3 regions 
-        # regions = {
-        #     'left': min(min(data.ranges[0:int(num_measurements*1/3)]), 10),
-        #     'front': min(min(data.ranges[int(num_measurements*1/3):int(num_measurements*2/3)]), 10),
-        #     'right': min(min(data.ranges[int(num_measurements*2/3):]), 10),
-        # }
-        regions = {
-            'left': min(min(data.ranges[0:int(num_measurements*1/6)]), 10),
-            'front': min(min(data.ranges[int(num_measurements*1/6):int(num_measurements*2/6)]), 10),
-            'right': min(min(data.ranges[int(num_measurements*2/6):int(num_measurements*3/6)]), 10),
-        }
+        except Exception as e:
+            rospy.logwarn(f"Incomplete or invalid data: {e}")
+            return 
 
-        rospy.loginfo(f"Regions: {regions}")
+
+        current_time = rospy.get_time()
+        if current_time - self.last_log_time >= self.log_frequency:
+            rospy.loginfo(f"Angle increment (degrees): {angle_increment_deg:.3f}")
+            rospy.loginfo(f"Number of measurements per 360-degree scan: {num_measurements:.0f}")
+            rospy.loginfo(f"Regions: {regions}")
+            self.last_log_time = current_time
 
         # Check the regions and decide the action 
         if regions['front'] < self.min_distance:
@@ -62,8 +76,15 @@ class ObstacleAvoidance:
         else:
             rospy.loginfo("Path is clear. Moving forward...")
 
-            # If no obstacle is detected in front move forward 
-            cmd_vel.linear.x = self.forward_speed
+            # Adjust speed based on the closest distance to an obstacle in the front region 
+            if (regions['front'] < self.max_distance):
+                # Scale the speed inversely proportional to distance 
+                cmd_vel.linear.x = self.forward_speed * (regions['front'] / self.max_distance)
+
+            else:
+            # Move at full speed
+                cmd_vel.linear.x = self.forward_speed
+    
             cmd_vel.angular.z = 0.0
 
         # Publish the velocity command 
